ee4be59592e26084ca0ad80b9a52d24aba2299b4
pengrui <pengrui@email.com>
2018-06-11 10:04:12 +0800

    Initial Commit

---
+++ SQRNetworkRequset/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SQRNetworkRequset
+//
+//  Created by macMini on 2018/6/11.
+//  Copyright © 2018年 PR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SQRNetworkRequset/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SQRNetworkRequset
+//
+//  Created by macMini on 2018/6/11.
+//  Copyright © 2018年 PR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
